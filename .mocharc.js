104586ca10d221082e0b605ce838bab55296d037
NVKPAVANKUMAR <npavankumar144@gmail.com>
2021-09-11 12:31:01 +0530

    mocharc.js file renamed

--- .mocharc.js
+++ .mocharc.js
@@ -1,11 +1,15 @@
 module.exports = {
-  spec: ['test/*.test.js'],
-  reporter: 'node_modules/mochawesome',
+  spec: ['./test/*'],
+  reporter: 'mochawesome',
   'reporter-option': [
     'overwrite=true',
     'reportTitle=Open Weather API Tests',
     'showPassed=false',
     'timestamp=isoDateTime',
+    'html=true',
+    'json=false',
+    'charts=true',
+    'recursive=true',
   ],
-  timeout: 5000,
+  timeout: 10000,
 };
